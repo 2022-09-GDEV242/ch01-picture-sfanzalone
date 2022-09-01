969c3027ffa9ab1b74a04c425d51bdbd5c01085c
sfanzalone <sfanzalone@gmail.com>
2022-09-01 13:50:27 -0400

    Wrote code in wrong ".java" file; transferred written code to "Picture.java"

--- Picture.java
+++ Picture.java
@@ -1,84 +1,114 @@
+Picture.java
+
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
+ * Canvas is a class to allow for simple graphical drawing on a canvas.
+ * This is a modification of the general purpose Canvas, specially made for
+ * the BlueJ "shapes" example.
  *
- * This class was written as an early example for teaching Java with BlueJ.
- * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ *The picture will present a simple representation of a person working on a
+ *computer using some simple shapes.
+ *
+ * @author: Salvatore Anzalone
+ * @version: 08.31.2022
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
-    private Circle sun;
-    private boolean drawn;
+    private Square table;
+    private Square comPiece1;
+    private Square comPiece2;
+    private Square comScreen;
+    private Triangle compitr;
+    private Circle mouse;
+    private Square background;
+    private Person comuser;
+
+    private static Canvas canvasSingleton;
 
     /**
-     * Constructor for objects of class Picture
+     * Constructor for objects of class PictureAnzalone
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
-        sun = new Circle();
-        drawn = false;
+        table =  new Square();
+        background =  new Square();
+        comPiece1 =  new Square();
+        comPiece2 =  new Square();
+        comScreen =  new Square();
+        compitr =  new Triangle();
+        mouse = new Circle();
+        comuser =  new Person();
     }
 
-    /**
-     * Draw this picture.
+  /**
+     * Draw picture.
      */
     public void draw()
     {
-        if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.makeVisible();
-            
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
-    
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
-    
-            sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
-            sun.changeSize(80);
-            sun.makeVisible();
-            drawn = true;
-        }
+        table.changeColor("grey");
+        table.moveHorizontal(-320);
+        table.changeSize(225);
+        table.makeVisible();
+
+        background.changeColor("white");
+        background.moveHorizontal(-340);
+        background.moveVertical(-550);
+        background.changeSize(550);
+        background.makeVisible();
+
+        comPiece1.changeColor("black");
+        comPiece1.moveHorizontal(-165);
+        comPiece1.moveVertical(-55);
+        comPiece1.changeSize(90);
+        comPiece1.makeVisible();
+
+        comPiece2.changeColor("black");
+        comPiece2.moveHorizontal(-85);
+        comPiece2.moveVertical(-55);
+        comPiece2.changeSize(95);
+        comPiece2.makeVisible();
+
+        comScreen.changeColor("skyblue");
+        comScreen.moveHorizontal(-85);
+        comScreen.moveVertical(-55);
+        comScreen.changeSize(90);
+        comScreen.makeVisible();
+
+        compitr.changeColor("black");
+        compitr.moveHorizontal(-80);
+        compitr.moveVertical(-55);
+        compitr.changeSize(40);
+        compitr.makeVisible();
+
+        mouse.changeColor("black");
+        mouse.moveHorizontal(-170);
+        mouse.moveVertical(-60);
+        mouse.changeSize(70);
+        mouse.makeVisible();
+
+        comuser.changeColor("lightgrey");
+        comuser.changeSize(80,40);
+        comuser.moveHorizontal(-94);
+        comuser.moveVertical(25);
+        comuser.makeVisible();
+
     }
 
     /**
-     * Change this picture to black/white display
+     * Change picture to black/white display
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
-        sun.changeColor("black");
+        if(background != null) //only if already painted
+        {
+            table.changeColor("darkgrey");
+            background.changeColor("grey");
+            comPiece1.changeColor("black");
+            comPiece2.changeColor("black");
+            comScreen.changeColor("white");
+            compitr.changeColor("black");
+            mouse.changeColor("black");
+            comuser.changeColor("lightgrey");
+        }
     }
 
-    /**
-     * Change this picture to use color display
-     */
-    public void setColor()
-    {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
-        sun.changeColor("yellow");
-    }
 }
