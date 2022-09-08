b57061401fe1691b10b76e5482261acc00c023f5
sfanzalone <sfanzalone@gmail.com>
2022-09-08 13:29:45 -0400

    Went back and recolored ComPiece1

--- Square2.java
+++ Square2.java
@@ -23,7 +23,7 @@ public class SquareComPiece1
         size = 60;
         xPosition = 310;
         yPosition = 120;
-        color = "red";
+        color = "black";
         isVisible = false;
     }
 
